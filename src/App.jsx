2758a3c281be5531067ea6e615e5bd2a709e0a88
llynoirx <kathyho04@gmail.com>
2024-10-03 01:40:13 -0400

    make card pairs (Q&A)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import { useState } from 'react';
 
 const App = () => {
 
+  const cardPairs = [
+    {Q: "Amendment 1", A: "Freedom of Religion, Speech, Press, Assembly, and Petition"},
+    {Q: "Amendment 2", A: "Right to bear arms"},
+    {Q: "Amendment 3", A: "Restrict Quartering of soldiers"},
+    {Q: "Amendment 4", A: "Prohibit Search and Seizures"},
+    {Q: "Amendment 5", A: "Right to Due Process"},
+    {Q: "Amendment 6", A: "Right to Speedy Trial"},
+    {Q: "Amendment 7", A: "Right to a Jury"},
+    {Q: "Amendment 8", A: "Prohibt Excessive Fines and Cruel and Unusual Punishment"},
+    {Q: "Amendment 9", A: "Rights not in the Constitution are retained by the People"},
+    {Q: "Amendment 10", A: "Federal Govt only has powers through Constitution; all other powers for the states"},
+  ]
+
   return (
     <div className="App">
       <h1>Bill of Rights</h1>
